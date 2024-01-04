2c0de39ad94b48c4f05a27c68cf1e18a7ac714ea
Ho Huu Duc <66152949+27hohuuduc@users.noreply.github.com>
2024-01-04 21:24:50 +0700

    Add files via upload

---
+++ runmongodb.bat
@@ -0,0 +1,5 @@
+set "params=%*"
+cd /d "%~dp0" && ( if exist "%temp%\getadmin.vbs" del "%temp%\getadmin.vbs" ) && fsutil dirty query %systemdrive% 1>nul 2>nul || (  echo Set UAC = CreateObject^("Shell.Application"^) : UAC.ShellExecute "cmd.exe", "/k cd ""%~sdp0"" && ""%~s0"" %params%", "", "runas", 1 >> "%temp%\getadmin.vbs" && "%temp%\getadmin.vbs" && exit /B )
+
+cd C:\Program Files\MongoDB\Server\7.0\bin\
+mongod.exe --dbpath="d:\mongodb\data\db"
